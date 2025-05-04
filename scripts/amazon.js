4a3134cfee1419af6ad032fcbfdc190d94f9ae54
piyush <piyush.khadka2233@gmail.com>
2025-05-04 06:30:21 +0100

    made add to cart interactive

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -47,10 +47,45 @@ products.forEach((product) =>{
         Added
       </div>
 
-      <button class="add-to-cart-button button-primary">
+      <button class="add-to-cart-button button-primary js-add-to-cart"
+      data-product-id="${product.id}">
         Add to Cart
       </button>
     </div>`;
 });
 
 document.querySelector('.js-products-grid').innerHTML = productsHTML;
+document.querySelectorAll('.js-add-to-cart')
+.forEach((button) => {
+  button.addEventListener('click', () => {
+    const productid = button.dataset.productid;
+
+    let matchingItem;
+    cart.forEach((item) => {
+      if (productid === item.productid) {
+        matchingItem = item;
+      }
+    })
+    if (matchingItem) {
+      matchingItem.quantity += 1;
+    } else {
+      cart.push({
+        productid: productid,
+        quantity: 1,
+      });
+    }
+    console.log(cart);
+  });
+  
+})
+
+
+
+  
+// });
+
+  // .forEach((button) =>{
+  //   button.addEventListener("click", () =>{
+  //       asdlkfj
+  //   });
+  // })
