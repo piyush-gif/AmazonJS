dc62918898e65144b3b57b6334ce4878abf70d4f
piyush <piyush.khadka2233@gmail.com>
2025-05-04 07:00:37 +0100

    move addtoCart to cart.js

--- data/cart.js
+++ data/cart.js
@@ -1,1 +1,18 @@
 export const cart = [];
+
+export function addToCart(productid) {
+  let matchingItem;
+  cart.forEach((cartItem) => {
+    if (productid === cartItem.productid) {
+      matchingItem = cartItemtem;
+    }
+  })
+  if (matchingItem) {
+    matchingItem.quantity += 1;
+  } else {
+    cart.push({
+      productid: productid,
+      quantity: 1,
+    });
+  }
+}
