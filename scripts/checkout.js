0cc077d143553028643d09ff955342aae4164a47
piyush <piyush.khadka2233@gmail.com>
2025-05-06 13:44:00 +0100

    used promises

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -2,7 +2,13 @@ import {renderOrderSummary} from './checkout/orderSummary.js';
 import { renderPaymentSummary } from './checkout/paymentSummary.js';
 import { loadProducts } from '../data/products.js';
 // import '../data/cart-class.js';
-loadProducts(() => {
+
+new Promise((resolve) => {
+  loadProducts(() => {
+    resolve();
+  })
+}).then(() => {
   renderOrderSummary();
   renderPaymentSummary();
-});
+})
+
